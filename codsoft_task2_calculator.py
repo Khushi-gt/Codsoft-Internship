6b333c2f69ac9930af94376168ceffb1e46b0a95
Khushi-gt <210814@kit.ac.in>
2024-07-28 12:38:32 +0530

    Update codsoft_task2_calculator.py

--- codsoft_task2_calculator.py
+++ codsoft_task2_calculator.py
@@ -1,7 +1,7 @@
 #COMMAND LINE APPLICATION OF CALCULATOR
 
-#Author : Jatoth Adithya Naik
-#for    : Intenship (TASK-2)
+#Author : Khushi Gupta
+#for    : Codsoft (TASK-2)
 
 # Discription :
 # this command appplication is for performing the arthmetic operations
@@ -10,7 +10,7 @@
 result =0
 
 def main():
-        print("\n\n\t\t\tby @JATOTH ADITHYA NAIK")
+        print("\n\n\t\t\tby @Khushi Gupta")
         print("________________________________________")
         print("\n\t\t\t***CALCULATOR APP***\n")
         while True :
